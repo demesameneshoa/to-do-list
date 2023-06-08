20e8ca95966a6ecd90a3b9b450cbedcd7a374bd2
demesameneshoa <demesameneshoa@gmail.com>
2023-06-08 14:42:06 +0300

    Deployed list add,update,read and delete functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,55 @@
 // Importing CSS styles
 import './style.css';
-import render from './modules/displaylist.js';
+import {
+  render,
+  addTask,
+  editTask,
+  removeTask,
+} from './modules/handletodo.js';
 
-// Adding main component to the HTML file
-const mainContainer = document.querySelector('.main');
-mainContainer.appendChild(render());
+render();
+// add task when text is submitted
+const taskinput = document.querySelector('.task-desc');
+taskinput.addEventListener('change', (e) => {
+  e.preventDefault();
+  if (taskinput !== '') {
+    const taskdesc = taskinput.value;
+    addTask(taskdesc);
+    taskinput.value = taskinput.ariaPlaceholder;
+  }
+});
+
+// make task list editable with click
+const tasks = document.querySelectorAll('.list-element');
+tasks.forEach((task) => {
+  task.addEventListener('click', () => {
+    task.contentEditable = true;
+    task.addEventListener('focus', () => {
+      const iconMenu = task.lastChild;
+      const icon = iconMenu.lastChild;
+      icon.classList.add('fa-trash');
+      const delteteTask = document.querySelector('.fa-trash');
+      if (delteteTask !== null) {
+        delteteTask.addEventListener('click', () => {
+        // get index of the task to delete from parent node
+          const delteIndex = task.firstChild.firstChild.getAttribute('id');
+          removeTask(delteIndex);
+        // console.log(task.firstChild.firstChild);
+        });
+      }
+    });
+  });
+});
+
+const taskblur = document.querySelectorAll('.list-element');
+taskblur.forEach((task) => {
+  task.addEventListener('blur', () => {
+    // task.contentEditable=true;
+    const child1 = task.firstChild;
+    const child2 = child1.lastChild;
+    const editedtext = child2.innerText;
+    const editedIndex = child2.getAttribute('id');
+    editTask(editedtext, editedIndex);
+    window.location.reload();
+  });
+});
